bb21ccef027fdf4b59c0f528c8ac70d42683c8af
Jaehyuk Choi <github@jae.eml.cc>
2017-09-28 01:31:28 +0800

    File rename

---
+++ option_models/basket.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 22:56:58 2017
+
+@author: jaehyuk
+"""
+import numpy as np
+import scipy.stats as ss
+
+def basket_check_args(spot, vol, corr_m, weights):
+    '''
+    This function simply checks that the size of the vector (matrix) are consistent
+    '''
+
+def basket_price_mc_cv(
+    strike, spot, vol, weights, texp, cor_m, 
+    intr=0.0, divr=0.0, cp_sign=1, n_samples = 10000
+):
+    # price1 = MC based on BSM
+    price1 = basket_price_mc(
+        strike, spot, vol, weights, texp, cor_m,
+        intr, divr, cp_sign, True, n_samples)
+    
+    ''' 
+    compute price2 and price3 based on normal model
+    '''
+    price2 = 0
+    price3 = 0
+    
+    return price1 - (price2 - price3)
+    
+    
+def basket_price_mc(
+    strike, spot, vol, weights, texp, cor_m,
+    intr=0.0, divr=0.0, cp_sign=1, bsm=True, n_samples = 10000
+):
+    div_fac = np.exp(-texp*divr)
+    disc_fac = np.exp(-texp*intr)
+    forward = spot / disc_fac * div_fac
+
+    cov_m = vol * cor_m * vol[:,None]
+    chol_m = np.linalg.cholesky(cov_m)
+
+    n_assets = spot.size
+    znorm_m = np.random.normal(size=(n_assets, n_samples))
+    
+    if( bsm ) :
+        '''
+        put your code here
+        '''
+        pass
+    else:
+        prices = forward[:,None] + np.sqrt(texp) * chol_m @ znorm_m
+    
+    price_weighted = weights @ prices
+    
+    price = np.mean( np.fmax(cp_sign*(price_weighted - strike), 0) )
+    return disc_fac * price
+
+def basket_price_norm_analytic(
+    strike, spot, vol, weights, 
+    texp, cor_m, intr=0.0, divr=0.0, cp_sign=1
+):
+    
+    '''
+    1. compute the forward of the basket
+    2. compute the normal volatility of basket
+    3. plug in the forward and volatility to the normal price formula
+    you may copy & paste the normal price formula from your previous HW.
+    '''
+    
+    return 0.0
